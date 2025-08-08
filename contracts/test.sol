b312958337d5034c714b84e2ec53525afc28e92d
02-is-02 <weihaowu89@gmail.com>
2025-08-09 02:32:55 +0800

    backup

--- contracts/test.sol
+++ contracts/test.sol
@@ -7,10 +7,19 @@ contract test {
   string message;
   constructor() public 
   {
+	owner = msg.sender;
     message = "Hello World!";
   }
   
+	address public owner;
+
+	uint256 public constant fee = 0.01 ether;
+
+	using Strings for string;
+	using Strings for uint256;
+
 	struct productDetail {
+	string manufacturerName;
 	string productName;
 	uint256 creationTime;
 	address currentHolder;
@@ -18,55 +27,99 @@ contract test {
 	string[] pathRecord;
 	}
 
-	enum role {Manufacturer, Middleman, Consumer}
-	enum DAStatus {Dispatch, Accept}
-	//the first level string always be product manufacturer
-	mapping(string => mapping(bytes32 => productDetail)) products;
-	//the second level string is product name
+	struct accountDetail {
+		string role;
+		string name;
+		string location;
+	}
+
+	mapping(address => accountDetail) accounts;
+
+	mapping(bytes32 => productDetail) products;
+
 	mapping(string => mapping(string => uint)) productCounts;
 
-	function NewInstance(string memory pName, string memory mName, string memory location, address mAddress) public returns (bytes32)
+	modifier onlyOwner() 
 	{
-		productDetail memory pNew = productDetail(pName, block.timestamp, mAddress, false, new string[](0));
-		bytes32 idNew = keccak256(abi.encodePacked(pNew.productName, pNew.creationTime, (productCounts[mName][pName] + 1)));
-		products[mName][idNew] = pNew;
-		
-		productCounts[mName][pName]++;
-		return idNew;
-	}
+	require(msg.sender == owner, "Only owner can call this function");
+	_;
+    }
 
-	function Transfer(string memory mName, bytes32 pID, string memory acceptRole, string memory location, address acceptAddress) public returns (bool)
+	receive() external payable {}
+
+	function Register(address rAdd, string memory role, string memory name, string memory location) public returns (bool) 
 	{
-	if (products[mName][pID].isDelivered)
-	{
-		return false;
-	}
-	if (isEqualString(acceptRole, "Consumer"))
-	{
-		products[mName][pID].currentHolder = acceptAddress;
-		products[mName][pID].isDelivered = true;
-		products[mName][pID].pathRecord.push(string.concat(acceptRole, location, "Accept", block.timestamp.toString()));
+		accountDetail memory aNew = accountDetail(role, name, location);
+		accounts[rAdd] = aNew;
 		return true;
 	}
-	else if (isEqualString(acceptRole, "Middleman"))
+
+	function NewInstance(address mAddress, string memory pName) public returns (bytes32)
 	{
-		products[mName][pID].currentHolder = acceptAddress;
-		products[mName][pID].pathRecord.push(string.concat(acceptRole, location, "Accept", block.timestamp.toString()));
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+		if (!accounts[mAddress].role.equal("Manufacturer"))
+		{
+			return bytes32(0);
+		}
+		else
+		{
+			string[] memory initPath = new string[](0);
+			initPath[0] = string.concat(accounts[mAddress].name, ",", accounts[mAddress].location, ",", block.timestamp.toString());
+			productDetail memory pNew = productDetail(accounts[mAddress].name, 
+			pName, 
+			block.timestamp, 
+			mAddress, 
+			false, 
+			initPath
+			);
+			bytes32 idNew = keccak256(abi.encodePacked(pNew.productName, pNew.creationTime, (productCounts[accounts[mAddress].name][pName] + 1)));
+			products[idNew] = pNew;
+			productCounts[accounts[mAddress].name][pName]++;
+			return idNew;
+		}
 	}
 
-	function isEqualString(string memory a, string memory b) internal pure returns (bool) 
+	function Transfer(address acceptAddress, bytes32 pID) public returns (bool)
 	{
-	return keccak256(bytes(a)) == keccak256(bytes(b));
+		if (products[pID].isDelivered || !accounts[acceptAddress].role.equal("Middleman"))
+		{
+			return false;
+		}
+		else
+		{
+			products[pID].currentHolder = acceptAddress;
+			products[pID].pathRecord.push(string.concat(accounts[acceptAddress].role, ",", accounts[acceptAddress].location,",", block.timestamp.toString()));
+			return true;
+		}
 	}
 
-	function isEqualBytes(bytes memory a, bytes memory b) internal pure returns (bool) 
+	function Purchase(address acceptAddress, bytes32 pID) public payable returns (bool) 
 	{
-	return keccak256(a) == keccak256(b);
+		if (products[pID].isDelivered)
+		{
+			return false;
+		}
+		else
+		{
+			products[pID].currentHolder = acceptAddress;
+			products[pID].isDelivered = true;
+			products[pID].pathRecord.push(string.concat("Consumer,", accounts[acceptAddress].location,",", block.timestamp.toString()));
+			return true;
+		}
 	}
+
+	function Payment() public payable
+	{
+		require(msg.value == fee, "Incorrect fee amount");
+		// put display here?
+	} 
+
+	function Withdraw() public onlyOwner 
+	{
+        uint256 balance = address(this).balance;
+
+        require(balance > 0, "Empty balance");
+
+        (bool success, ) = owner.call{value: balance}("");
+        require(success, "Withdrawal failed");
+    }
 }
